69bb2ef6dff3eae23462d74fe47e1653103f61c5
hhimanshu <dev.atwork@yahoo.com>
2023-01-04 11:11:07 -0800

    feat(): insert user with posts and fetch them by including it

--- typescript/hhimanshu/sqlite-get-started/README.md
+++ typescript/hhimanshu/sqlite-get-started/README.md
@@ -4,3 +4,4 @@ The documentation followed is [here](https://www.prisma.io/docs/getting-started/
 1. Clone repo
 2. `npx prisma generate`
 3. `npx ts-node script.ts`
+4. Browse data using command `npx prisma studio`
--- typescript/hhimanshu/sqlite-get-started/script.ts
+++ typescript/hhimanshu/sqlite-get-started/script.ts
@@ -14,12 +14,33 @@ async function main() {
   }
 
   const getAllUsers = async () => {
-    const users = await prisma.user.findMany()
-    console.log(users)
+    const users = await prisma.user.findMany({
+      include: {
+        post: true
+      }
+    })
+    console.dir(users, {depth: null})
+  }
+
+  const createUserWithPosts = async () => {
+    const user = await prisma.user.create({
+      data: {
+        name: 'Johnny D',
+        email: 'johnny@email.com',
+        post: {
+          create: {
+            title: 'First Post',
+            content: 'This is my first post'
+          }
+        }
+      }
+    })
+    console.log(user)
   }
 
   //await insertUser()
   await getAllUsers()
+  //await createUserWithPosts()
 }
 
 main()
